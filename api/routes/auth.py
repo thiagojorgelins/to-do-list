287bbcdccd2341530b6b625372e28ebc6c187c2c
thiagojorgelins <thiago.jorge2001@gmail.com>
2024-11-04 01:31:41 -0300

    feat: add username & user_id to JWT Payload

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -22,6 +22,6 @@ async def login_for_access_token(
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.email }, expires_delta=access_token_expires
+        data={"sub": user.email, "username": user.username, "user_id": user.id }, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "Bearer"}
