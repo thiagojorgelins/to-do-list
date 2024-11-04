9775610d02f8a9a40c790fc0d0e1f514ae43e5cf
thiagojorgelins <thiago.jorge2001@gmail.com>
2024-11-04 00:15:37 -0300

    feat: add schemas(User, Tasks & Token)

---
+++ api/schemas.py
@@ -0,0 +1,67 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+
+class User(UserBase):
+    id: int
+    created_at: datetime
+    updated_at: Optional[datetime]
+
+    class Config:
+        from_attributes = True
+
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+from enum import Enum
+
+class TaskStatus(str, Enum):
+    PENDING = 'Pendente'
+    IN_PROGRESS = 'Em andamento'
+    COMPLETED = 'Concluída'
+
+class TaskBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    status: TaskStatus = TaskStatus.PENDING
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    status: Optional[TaskStatus] = None
+
+class Task(TaskBase):
+    id: int
+    created_at: datetime
+    updated_at: Optional[datetime]
+    user_id: int
+
+    class Config:
+        from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
