a647101ed05e20201c4a262e2af4a3431c41dd3a
thiagojorgelins <thiago.jorge2001@gmail.com>
2024-11-04 00:23:31 -0300

    feat: add User endpoinsts (CRUD)

---
+++ api/routes/users.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter, Depends, HTTPException
+from http import HTTPStatus
+from sqlalchemy.orm import Session
+from api.database import get_db
+from api.security import get_password_hash
+from .. import models, schemas
+
+router = APIRouter()
+
+@router.post("/users/", response_model=schemas.User)
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(
+        models.User.email == user.email
+    ).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(
+        email=user.email,
+        username=user.username,
+        password=hashed_password
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@router.put("/users/{user_id}", response_model=schemas.User)
+async def update_user(
+    user_id: int,
+    user_update: schemas.UserUpdate,
+    db: Session = Depends(get_db)
+):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    if user_update.username:
+        db_user.username = user_update.username
+    if user_update.email:
+        db_user.email = user_update.email
+    if user_update.password:
+        db_user.password = get_password_hash(user_update.password)
+    
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+@router.delete("/users/{user_id}")
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    db.delete(db_user)
+    db.commit()
+    return {"detail": "User deleted"}
